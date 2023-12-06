700cec18bd309d0e6c9dd3ed248a4291eb6fb73d
Vault-Obsidian-ThiagoLima <thiagolima120603@outlook.com>
2023-12-06 16:07:33 -0300

    Criei uma variável global de conexão com o banco de dados e um método que retorna o tipo em linguagem SQL para os tipos C#

--- Estoque/Database/Base.cs
+++ Estoque/Database/Base.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Reflection;
 using System.Text;
 
 namespace Database
 {
-    class Base
+    public class Base : IBase
     {
+        private string ConnectionString = ConfigurationManager.AppSettings["SqlConnection"];
+
+        /// <summary>
+        /// Faz uma espécie de conversão de string pegando os tipos do C# e transformando e tipos SQL
+        /// </summary>
+        public string TipoPropriedade(PropertyInfo pi)
+        {
+            switch (pi.PropertyType.Name)
+            {
+                case "Int32":
+                    return "INT";
+                case "Int64":
+                    return "BIGINT";
+                case "Double":
+                    return "DECIMAL(9,2)";
+                case "Single":
+                    return "FLOAT";
+                case "DateTime":
+                    return "DATETIME";
+                case "Boolean":
+                    return "TINYINT";
+                default:
+                    return "VARCHAR(255)";
+            }
+        }
     }
 }
