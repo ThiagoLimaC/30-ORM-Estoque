aba6c68065a681c74a1b07958656719ba4de38ac
Vault-Obsidian-ThiagoLima <thiagolima120603@outlook.com>
2023-12-06 17:03:50 -0300

    Criei a classe Produto

---
+++ Estoque/Business/Produto.cs
@@ -0,0 +1,19 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public class Produto : Base
+    {
+        [OpcoesBase(UsarNoBancoDeDados = true, ChavePrimaria = true, UsarParaBuscar = true)]
+        public string IdProd { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public string Nome { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public string Descricao { get; set; }
+    }
+}
