3681733a6c9cda1555a8dbb6f7558cee6448085d
Vault-Obsidian-ThiagoLima <thiagolima120603@outlook.com>
2023-12-07 00:03:55 -0300

    Tive problemas com o checkout do github, e tive que excluir o formulário 2

--- Estoque/UI/Form2.cs
+++ Estoque/UI/Form2.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UI
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
